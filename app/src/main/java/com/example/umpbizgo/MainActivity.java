ca856f8f896dce738d7fa6e37cd3c1338cbd7fcd
maheshrao98 <raomahesh610@gmail.com>
2020-09-02 13:10:28 +0800

    Splash Screen with Main Activity

---
+++ app/src/main/java/com/example/umpbizgo/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.umpbizgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.umpbizgo.Adapters.PlateAdapter;
+import com.example.umpbizgo.Models.PlateModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<PlateModel> plateModelList;
+    private PlateAdapter plateAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ///// Hide Status Bar Start//////
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        ///// Hide Status Bar End//////
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setKeepScreenOn(true);
+        recyclerView.setHasFixedSize(true);
+
+        plateModelList = new ArrayList<>();
+        plateModelList.add(new PlateModel(R.drawable.foodplate));
+        plateModelList.add(new PlateModel(R.drawable.foodplate));
+        plateModelList.add(new PlateModel(R.drawable.foodplate));
+        plateModelList.add(new PlateModel(R.drawable.foodplate));
+        plateModelList.add(new PlateModel(R.drawable.foodplate));
+        plateModelList.add(new PlateModel(R.drawable.foodplate));
+        plateModelList.add(new PlateModel(R.drawable.foodplate));
+        plateModelList.add(new PlateModel(R.drawable.foodplate));
+
+        plateAdapter = new PlateAdapter(plateModelList,this);
+        recyclerView.setAdapter(plateAdapter);
+        plateAdapter.notifyDataSetChanged();
+
+        ////////////call autoscroll start////////////////
+        autoScroll();
+        ////////////call autoscroll end/////////////////
+    }
+
+    public void autoScroll(){
+        final int speedScroll = 0;
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            int count = 0;
+            @Override
+            public void run() {
+                if(count == plateAdapter.getItemCount())
+                    count = 0;
+                if (count < plateAdapter.getItemCount()){
+                    recyclerView.smoothScrollToPosition(++count);
+                    handler.postDelayed(this,speedScroll);
+                }
+            }
+        };
+        handler.postDelayed(runnable,speedScroll);
+    }
+}
---
+++ app/src/main/java/com/example/umpbizgo/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.umpbizgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ////Status bar hider////
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        ////Status bar hider ends////
+
+        ////Handler Start////
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+        ////Handler End//////
+    }
+}
